347c5d3f06d2e874367e0411e9a6b261d42182ea
Michael Bradshaw <github@mjb.io>
2022-08-26 13:47:56 -0600

    Guess the number

---
+++ README.md
@@ -0,0 +1,1 @@
+See https://internals.rust-lang.org/t/minigames-while-waiting-for-builds/17273
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+// A terrible dependency-free hack.
+fn rand() -> u64 {
+    let now = std::time::SystemTime::now();
+    let some_number = dbg!(now.duration_since(std::time::UNIX_EPOCH).unwrap().as_millis()) as u64;
+	let mut xorshift = some_number;
+	xorshift ^= xorshift << 13;
+	xorshift ^= xorshift >> 7;
+	xorshift ^= xorshift << 17;
+    return dbg!(xorshift);
+}
+
+fn main() {
+    let number = dbg!(rand() % 101);
+    println!("Guess a number between 0 and 100");
+    loop {
+        let mut input_string = String::new();
+        std::io::stdin().read_line(&mut input_string).unwrap();
+        let input = input_string.trim().parse::<u64>().unwrap();
+        if input < number {
+            println!("Higher...");
+            continue;
+        }
+        if input > number {
+            println!("Lower...");
+            continue;
+        }
+        println!("Correct!");
+        break;
+    }
+}
